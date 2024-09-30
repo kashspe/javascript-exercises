ab5a258d9df684ed3885e09da39dacf0d06bbec9
kashspe <karim.himmi@gmail.com>
2024-10-01 00:09:22 +0200

    Exercise 2 completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,23 @@
-const repeatString = function() {
+// const repeatString = function(message, times) {
+//     for (let index = 0; index < times; index++) {
+//         let repeated = message;
+//         return repeated;
+//     }
+//         repeatString;   
+// };
 
-};
+function repeatString(message, times) {
+    if (times < 0) {
+        return "ERROR"
+    }
+    let returnMessage = ""
+    for (let index = 0; index < times; index++) {
+        returnMessage +=  message
+        
+    } return (returnMessage)  
+}
+   
+
 
 // Do not edit below this line
 module.exports = repeatString;
