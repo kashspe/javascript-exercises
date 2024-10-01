583248779a2aa2086dfcbac6a6d3faf2dc7ba3b5
kashspe <karim.himmi@gmail.com>
2024-10-01 16:29:28 +0200

    Exercise 4 completed
    rest, filter  methods used

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
-};
+// const removeFromArray = function() {
+// };
+function removeFromArray( array, ...toSupp) {
+    return array.filter(item => !toSupp.includes(item)); 
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
