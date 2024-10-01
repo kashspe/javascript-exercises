af8690b559b6ef89f138234a67a633ee636cb821
kashspe <karim.himmi@gmail.com>
2024-10-01 13:22:40 +0200

    Exercise 3 completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,15 @@
-const reverseString = function() {
+// const reverseString = function("stringToReverse") {
+//     let rever
+// };
 
-};
+function reverseString(word) {
+    let wordReversedArray = "";
+    for (let index = word.length; index > 0; index--) {
+        let a = word.charAt(index - 1);
+        wordReversedArray += a
+    }
+    return wordReversedArray;
+}
 
 // Do not edit below this line
 module.exports = reverseString;
