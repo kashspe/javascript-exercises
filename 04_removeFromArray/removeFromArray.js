8c6989c3d80193e880414b6fdf7c4c07b74fcd95
kashspe <karim.himmi@gmail.com>
2024-10-25 10:35:00 +0200

    Exercise 4 compeleted
    Remove from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,8 +1,25 @@
-// const removeFromArray = function() {
-// };
-function removeFromArray( array, ...toSupp) {
+ const removeFromArray = function(inputArray, ...toRemove) {
+    return inputArray.filter(verif)
+    function verif(elementToRemove) {
+        return !toRemove.includes(elementToRemove)
+    }
+    }
+ 
+/*function removeFromArray( array, ...toSupp) {
     return array.filter(item => !toSupp.includes(item)); 
 }
+*/
+// for (const item of inputArray) {
+//     if (item == toRemove) {
+//         inputArray.filter
+//     }
+// }
+// const inputArray = [45, 4, 9, 16, 25];
+// const toRemove =
+// const filteredArray = inputArray.filter(myFunction);
 
+// function myFunction(inputArray, toRemove) {
+//   return toRemove != inputArray;
+// }
 // Do not edit below this line
 module.exports = removeFromArray;
